706bef5c8a634790a5bd24d7863099f4d364587d
park jooeul <euleul90@gmail.com>
2022-09-07 17:02:24 +0900

    Initial commit

---
+++ src/main/java/com/pje/internetcafemanager/InternetCafeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pje.internetcafemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetCafeManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetCafeManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pje/internetcafemanager/InternetCafeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pje.internetcafemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetCafeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
